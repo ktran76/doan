7ac5d8d7a4df2e17a76efbaa81b1a1972372d0c2
xboyss <tkhoi7366@gmail.com>
2023-04-29 15:20:31 +0700

    update

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,36 +1,19 @@
 package controllers;
 
-import dto.UserDto;
-import entity.User;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.Session;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import services.UserService;
-
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Objects;
+import java.io.IOException;
 
 
 @Slf4j
 @Controller
 public class HomeController {
-    @Autowired
-    UserService userService;
 
 
 
     @RequestMapping("/")
-    public String hello(@RequestParam(required = false) String username, Model model){
-        List<User> users = userService.getAllUser();
-        for (User user: users) {
-            System.out.println(user.getUsername());
-        }
+    public String hello() throws IOException {
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,10 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-</body>
-</html>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<div>
+    <c:forEach var="username" items="${usersname}">
+        <h2>${username}</h2>
+    </c:forEach>
+</div>
